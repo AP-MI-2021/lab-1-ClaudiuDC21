8098090c6962a17a5409b130ded937521736c210
Claudiu <claudiu.dascalu@stud.ubbcluj.ro>
2021-10-11 12:37:42 +0300

    Functioneaza

--- main.py
+++ main.py
@@ -1,33 +1,62 @@
-xsxsx'''
-Returneaza true daca n este prim si false daca nu.
 '''
+ Returneaza true daca n este prim si false daca nu.
+'''
+
+
 def is_prime(n):
-  # codul vost
-  
-  
+    if n < 2:
+        return False
+    for i in range(2, n - 1):
+        if n % i == 0:
+            return False
+    return True
+
+
 '''
 Returneaza produsul numerelor din lista lst.
 '''
+
+
 def get_product(lst):
-  # codul vostru aici
-  
-  
+    product = 1
+    for x in lst:
+        product = product * x
+    return product
+
+
 '''
 Returneaza CMMDC a doua numere x si y folosind primul algoritm.
 '''
+
+
 def get_cmmdc_v1(x, y):
-  # codul vostru aici
-  
-  
+    while x != y:
+        if x > y:
+            x = x - y
+        else:
+            y = y - x
+    return x
+
+
 '''
 Returneaza CMMDC a doua numere x si y folosind al doilea algoritm.
 '''
+
+
 def get_cmmdc_v2(x, y):
-  # codul vostru aici
-  
-  
+    while y != 0:
+        z = x % y
+        x = y
+        y = z
+    return x
+
+
 def main():
-  # interfata de tip consola aici
+    print(is_prime(7))
+    print(get_product([2, 3, 4, 5, 1]))
+    print(get_cmmdc_v1(24, 60))
+    print(get_cmmdc_v2(24, 60))
+
 
 if __name__ == '__main__':
-  main()
+    main()
